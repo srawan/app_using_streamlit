ff563ec1e11a697f73984001528833fae9dfd341
Srawan Singh <srawansingh.2009@rediffmail.com>
2025-02-17 09:57:42 +0000

    stream added

--- test_streamlit_app.py
+++ test_streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
+import time
 
 st.write("Hello, *World !* :sunglasses: ")
 st.write(1234)
@@ -23,3 +24,30 @@ c = (
     .encode(x="a", y ="b", size="c", color="c", tooltip=["a", "b", "c"])
 )
 st.write(c)
+
+st.write("Code for strean test")
+
+text = """
+    Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    """
+def stream_data():
+    for word in text.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+    yield pd.DataFrame(
+        np.random.randn(5,10),
+        columns=["a", "b", "c", "d", "e", "f", "g", "i", "j"]
+    )
+
+    for word in text.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+
+if st.button("Stream Data"):
+    st.write_stream(stream_data())
+
+
