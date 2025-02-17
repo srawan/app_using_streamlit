dcb135cfa32fe9c5e2bf99b8c1c3fcb4b0425f4d
Srawan Singh <srawansingh.2009@rediffmail.com>
2025-02-17 08:51:03 +0000

    first commit

--- test_streamlit_app.py
+++ test_streamlit_app.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
 
+st.write("Hello, *World !* :sunglasses: ")
+st.write(1234)
+st.write(
+pd.DataFrame (
+    {
+        "First column": [1,2,3,4],
+        "Second column":[10, 20, 30, 40]
+    }
+)
+)
